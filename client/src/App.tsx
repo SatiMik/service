bfc05c0002b771fbf19610218fd75a8b50e583de
SatiMik <sati62638@gmail.com>
2024-01-14 22:48:04 +0300

    final version

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,8 @@
 import { Container } from "@mui/material";
-// import Loader from "./components/hocs/Loader"
 import Navbar from "./components/ui/Navbar";
-// import { useAppSelector } from './redux/hooks';
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./components/pages/MainPage";
 import BrowsePage from "./components/pages/BrowsePage";
-// import PrivateRouter from "./components/hocs/PrivateRouter";
-// import AuthPage from "./components/pages/AuthPage";
 import LoginPage from "./components/pages/LoginPage";
 import PrivateRouter from "./components/hocs/PrivateRouter";
 import { useAppSelector } from "./redux/hooks";
@@ -30,8 +26,6 @@ function App(): JSX.Element {
           <Route element={<PrivateRouter isAllowed={user.data.status === 'logged'}  />}>
           <Route path="/browse" element={< BrowsePage />} />
           </Route> 
-
-
 
         </Routes>
       </Container>
--- client/src/components/pages/MainPage.tsx
+++ client/src/components/pages/MainPage.tsx
@@ -1,4 +1,3 @@
-import { Carousel } from 'react-bootstrap';
 
 export default function MainPage() {
     return (
@@ -6,7 +5,6 @@ export default function MainPage() {
         <h1 style={{ fontSize: '32px', color: '#2e3b55', marginBottom: '20px' }}>Добро пожаловать на наш сайт!</h1>
         <p style={{ fontSize: '18px', marginBottom: '10px' }}>Здесь вы найдете нужную вам информацию.</p>
         <p style={{ fontSize: '18px', marginBottom: '10px' }}>Исследуйте раздел сервисы после авторизации пользователя</p>
-        {/* <Link to="/articles" style={{ fontSize: '20px', color: '#2e3b55', textDecoration: 'none', padding: '10px 20px', border: '2px solid #2e3b55', borderRadius: '5px', transition: 'background-color 0.3s, color 0.3s' }}>Перейти к статьям</Link> */}
       </div>
   );
 }
